aff5e18e02b14295f0d1cb2ff0f0c84a42a6c328
Agustin <rosalesneyraagustin@gmail.com>
2025-08-27 20:36:54 -0300

    Orni Azul Metodos

--- src/Entidades/OrniAzul.java
+++ src/Entidades/OrniAzul.java
@@ -1,7 +1,6 @@
 package Entidades;
 import Entidades.Interfaces.Pata;
-
-
+import java.util.Scanner;
 public class OrniAzul extends Castor implements Pata {
     private int propulsion;
     public OrniAzul(int velocidad, int propulsion) {
@@ -10,8 +9,23 @@ public class OrniAzul extends Castor implements Pata {
     }
     //sobreescribimos el metodo del padre 
     @Override
-    public void nadar
-            
+    public void nadar(){
+       int velTotal=  super.getVelocidad()+ propulsion;//sumamos la velocidad del padre mas la velocidad de propulsion
+        System.out.println("el ornitorrinco nada a una velocidad de:" + velTotal);
     }
     
+    @Override
+    public void tocarGuitarra(){
+        int numR = (int) (Math.random()*6)+0;
+    System.out.println("Toca cuerda: "+ numR);
+}
+
+    @Override
+    public void tocarOrgano() {
+        Scanner oprimir = new Scanner(System.in);
+        System.out.println("Do - Re - Mi");
+        oprimir.nextLine();//se espera a que presiona enter para mostrar el siguiente sout
+        System.out.println("Fa - Sol - La - Si");
+    }
+  
 }
