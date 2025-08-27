9f06ca43483682aabc8c0a6fd2e9a3f11ec86cc8
Agustin <rosalesneyraagustin@gmail.com>
2025-08-27 20:19:36 -0300

    Agregue atributos y metodos al castor

--- src/Entidades/Castor.java
+++ src/Entidades/Castor.java
@@ -2,7 +2,19 @@
 package Entidades;
 
 public class Castor {
-    
-    
-    
+    //Atributos del castor
+    private int velocidad;//velocidad la ingresa el usuario
+    private String cola = "Ancha y plana";
+
+    public Castor(int velocidad){
+        this.velocidad = velocidad;
+    }
+    //metodos
+public void nadar(){
+    System.out.println("El castor nada a una velocidad de" + velocidad);
+}
+public void tocarGuitarra(){
+    System.out.println("Tocando la guitarra");
+}
+
 }
