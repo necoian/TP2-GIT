37076afd3a40b40e244d7c1361df606ca35666d1
necoian <0108.facultad@gmail.com>
2025-08-27 20:23:56 -0300

    interface PATA
    Se agregó los atributos y metodo a la interface Pata

--- src/Entidades/Interfaces/Pata.java
+++ src/Entidades/Interfaces/Pata.java
@@ -3,5 +3,8 @@ package Entidades.Interfaces;
 
 public interface Pata {
     
+    double LONG_PATAS = 7.5; //Longitud en centimetros
+    double LONG_PICO = 6.8; //Longitud en centimetros
     
+    void tocarOrgano(); //metodo a implementar
 }
