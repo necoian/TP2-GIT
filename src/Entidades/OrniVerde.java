14e6e3ad53baff62369c4dfe35f20c111356415a
necoian <0108.facultad@gmail.com>
2025-08-27 20:53:14 -0300

    cambio de orniazul a orniverde
    Cambio del color del ornitorrinco que pedia la consigna

--- src/Entidades/OrniVerde.java
+++ src/Entidades/OrniVerde.java
@@ -1,12 +1,33 @@
 package Entidades;
 import Entidades.Interfaces.Pata;
-
+import java.util.Scanner;
 public class OrniVerde extends Castor implements Pata {
-    
-    public OrniVerde(int velocidad) {
+    private int propulsion;
+    private String nombre;
+    public OrniVerde(int velocidad, int propulsion, String nombre) {
         super(velocidad);
+        this.propulsion = propulsion;
+        this.nombre = nombre;
+    }
+    //sobreescribimos el metodo del padre 
+    @Override
+    public void nadar(){
+       int velTotal=  super.getVelocidad()+ propulsion;//sumamos la velocidad del padre mas la velocidad de propulsion
+        System.out.println("el ornitorrinco nada a una velocidad de:" + velTotal);
     }
     
-    
-    
+    @Override
+    public void tocarGuitarra(){
+        int numR = (int) (Math.random()*6)+0;
+    System.out.println("Toca cuerda: "+ numR);
+}
+
+    @Override
+    public void tocarOrgano() {
+        Scanner oprimir = new Scanner(System.in);
+        System.out.println("Do - Re - Mi");
+        oprimir.nextLine();//se espera a que presiona enter para mostrar el siguiente sout
+        System.out.println("Fa - Sol - La - Si");
+    }
+  
 }
