caaf8a5856c8975301fe0ef67258862f989fc814
Agustin <rosalesneyraagustin@gmail.com>
2025-08-27 21:27:54 -0300

    modificaciones

--- src/TestMain.java
+++ src/TestMain.java
@@ -10,13 +10,14 @@ public class TestMain {
         Scanner leerT = new Scanner(System.in); 
        
         
-        System.out.println("Ingrese la velocidad del castor (hasta 8/km como maximo)");
+        System.out.println("Ingrese la velocidad del castor (hasta 8km/h como maximo)");
         int vel = leerT.nextInt();
         //instanciamos al papa Castor
         Castor c1 = new Castor(vel);
-        System.out.println("Ingrese la velocidad del ornitorrinco AZUL 1");
+        System.out.println("Ingrese la velocidad de propulsion(min 5km/h y  maximo 10km/h)");
+        System.out.println("Ornitorrinco AZUL 1");
         int vel2 = leerT.nextInt();
-        System.out.println("Ingrese la velocidad del ornitorrinco AZUL 1");
+        System.out.println("Ornitorrinco AZUL 2");
         int vel3 = leerT.nextInt();
         //instanciamos a los dos hermanos Ornitorrincos Azules
         OrniAzul o1 = new OrniAzul(vel, vel2,"Bluey");
