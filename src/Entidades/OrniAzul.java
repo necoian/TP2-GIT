a7305d93673cde8387b0a24209bc1c889f8d949f
Agustin <rosalesneyraagustin@gmail.com>
2025-08-28 16:04:55 -0300

    m

--- src/Entidades/OrniAzul.java
+++ src/Entidades/OrniAzul.java
@@ -1,25 +1,47 @@
 package Entidades;
-import Entidades.Interfaces.Pata;
+
+import java.util.Comparator;
 import java.util.Scanner;
+
 public class OrniAzul extends Castor {
-    private int propulsion;
+
+    private Integer propulsion;
     private String nombre;
+
     public OrniAzul(int velocidad, int propulsion, String nombre) {
         super(velocidad);
         this.propulsion = propulsion;
         this.nombre = nombre;
     }
-    //sobreescribimos el metodo del padre 
-    @Override
-    public void nadar(){
-       int velTotal=  super.getVelocidad()+ propulsion;//sumamos la velocidad del padre mas la velocidad de propulsion
-        System.out.println("el ornitorrinco nada a una velocidad de:" + velTotal);
+    
+    public Integer getPropulsion(){
+        return propulsion;
     }
     
+    //sobreescribimos los metodos del padre nadar() y tocarGuitarra().
     @Override
-    public void tocarGuitarra(){
-        int numR = (int) (Math.random()*6)+1;
-    System.out.println("Toca cuerda: "+ numR);
+    public void nadar() {
+        int velTotal = super.getVelocidad() + propulsion;//sumamos la velocidad del padre mas la velocidad de propulsion
+        System.out.println( nombre+" nada a una velocidad de: " + velTotal);
+    }
+
+    @Override
+    public void tocarGuitarra() {
+        int numR = (int) (Math.random() * 6) + 1;
+        System.out.println(nombre +" Toca cuerda: " + numR);
+    }
+    //Comparator de clase anonima
+    public static Comparator<OrniAzul> velocidadAscen= new Comparator<OrniAzul>() {
+        @Override
+        public int compare(OrniAzul o1, OrniAzul o2) {
+        
+            return o2.getPropulsion().compareTo(o1.getPropulsion());
+        }
+    };
+
+    @Override
+    public String toString() {
+        return "OrniAzul{" + "propulsion de nado=" + propulsion + ", nombre=" + nombre + '}';
     }
 
 }
--- src/TestMain.java
+++ src/TestMain.java
@@ -1,66 +1,76 @@
 
 import Entidades.Castor;
 import Entidades.OrniAzul;
-import java.util.LinkedHashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.Scanner;
-
 
 public class TestMain {
 
     public static void main(String[] args) {
-        Scanner leerT = new Scanner(System.in); 
-       
+        
+        Scanner leerT = new Scanner(System.in);
+        int vel, vel2, vel3;//VARIABLES
+        
         
         System.out.println("Ingrese la velocidad del castor ( desde 1 km/h hasta 8km/h como maximo)");
-        int vel = leerT.nextInt();
-        while(vel > 8 || vel < 1){ //si el usuario ingresa un valor fuera del rango entra al bucle
+        vel = leerT.nextInt();
+        while (vel > 8 || vel < 1) { //si el usuario ingresa un valor fuera del rango entra al bucle
             System.out.println("INGRESE Dentro del rango (1 km/h 8 km/h)");
             vel = leerT.nextInt();
         }
         //instanciamos al papa Castor
         Castor c1 = new Castor(vel);
-        
+
         System.out.println("Ingrese la velocidad de propulsion(min 5km/h y  maximo 10km/h)");
         System.out.println("Ornitorrinco AZUL 1");
-        int vel2 = leerT.nextInt();
-        while(vel2 > 10 || vel2 < 5){ //si el usuario ingresa un valor fuera del rango entra al bucle
+         vel2 = leerT.nextInt();
+        while (vel2 > 10 || vel2 < 5) { //si el usuario ingresa un valor fuera del rango entra al bucle
             System.out.println("Ingrese dentro del rango");
             vel2 = leerT.nextInt();
         }
         System.out.println("Ornitorrinco AZUL 2");
-        int vel3 = leerT.nextInt();
-        while(vel3 > 10 || vel3 < 5){ //si el usuario ingresa un valor fuera del rango entra al bucle
+       vel3 = leerT.nextInt();
+        while (vel3 > 10 || vel3 < 5) { //si el usuario ingresa un valor fuera del rango entra al bucle
             System.out.println("Ingrese dentro del rango");
             vel3 = leerT.nextInt();
         }
         //instanciamos a los dos hermanos Ornitorrincos Azules
-        OrniAzul o1 = new OrniAzul(vel, vel2,"Bluey");
-        OrniAzul o2 = new OrniAzul(vel, vel3,"Blue");
-        //metodo de nadar
-        System.out.println("bluey");
+        OrniAzul o1 = new OrniAzul(vel, vel2, "Bluey");
+        OrniAzul o2 = new OrniAzul(vel, vel3, "Blue");
+        System.out.println("...............................................");//separador 
+        //metodo de nadar y tocar guitarra de los ornitorrincos azules.
         o1.nadar();
-        System.out.println("blue");
         o2.nadar();
-        //metodo de tocarGuitarra
-        System.out.println("bluey");
-         o1.tocarGuitarra();
-        System.out.println("blue");
-         o2.tocarGuitarra();
-         //creamos arreglo
-        LinkedHashSet<Castor>ornitorrincos = new LinkedHashSet();
-        //Agregamos los ornitorrincos
-        try{
-          Castor[] ornit = new Castor[3];
-        //ornit[0] = ornitorrinco verde;
-          ornit[1] = o1;
-          ornit[2] = o2;
-        }catch(ArrayIndexOutOfBoundsException error){
-            System.out.println("Se salio del arreglo");
-        }finally{
+        o1.tocarGuitarra();
+        o2.tocarGuitarra();
+        try {
+        //Ejercicio de Arreglo de [3] con su try, catch y finally.
+            Castor[] ornit = new Castor[3];
+          //ornit[0] = ornitorrinco verde;
+            ornit[1] = o1;
+            ornit[2] = o2;
+        } catch (ArrayIndexOutOfBoundsException error) {
+            System.out.println("Se salio del arreglo!!!");
+        } finally {
             System.out.println("ornitohermanos juntos al fin");
         }
-       
-         
+        System.out.println("...............................................");//separador 
+        System.out.println("Ornitorrincos Azules ordenados segun velocidad de propulsion de forma Ascendente");
+        //creamos arreglo dinamico, para luego mostrarlo con iterator(OrniAzul!!)
+        ArrayList<OrniAzul> ornis = new ArrayList();
+        //Agregamos los ornitorrincos
+        ornis.add(o1);
+        ornis.add(o2);
+        //Mostramos a los ornitorrincos segun su velocidad de propulsion con iterador
+
+        Collections.sort(ornis, OrniAzul.velocidadAscen);
+        Iterator<OrniAzul> it = ornis.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
     }
-    
+
 }
